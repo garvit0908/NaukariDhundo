fde6cb2b3f80829b40625909dcc848544b572f2a
Garvit Mukhija <mukhijagarvit@gmail.com>
2023-09-30 14:00:46 +0530

    Api Calling Done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,37 @@
 import {useState} from 'react'
 import { View, Text ,TouchableOpacity,FlatList,ActivityIndicator } from 'react-native'
-import router from 'expo-router'
+import {useRouter} from 'expo-router'
+
+import { icons ,SIZES } from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 
 import styles from './popularjobs.style'
 
 const Popularjobs = () => {
+  const router=useRouter()
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+       <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+        </View>
+
+        <View style={styles.cardsContainer}>
+          <FlatList
+            data={[1,2,3,4,5,6,7,8,9,10]}
+            renderItem={({item})=>(
+              <PopularJobCard
+                 item={item}
+              />
+            )}
+            horizontal
+            contentContainerStyle={{columnGap:SIZES.medium}}
+          />
+        </View>
     </View>
   )
 }
